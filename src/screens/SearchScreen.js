92ac082d135678d8de71fb09fafbbdc9d5b6bdc7
edetebenezer <segunebenezer72@gmail.com>
2020-04-23 04:08:27 +0100

    converted splashscreen to use useState hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -8,19 +8,14 @@ import BookList from '../Components/BookList'
 import Loading from '../Components/Loading'
 
 const MAX_SEARCH_RESULT = 20
-export default class SearchScreen extends Component {
-  // const [query, ]
-  state = {
-    query: '',
-    searchResult: [],
-    isLoading: false,
-  }
+const SearchScreen = () => {
+  const [query, setQuery] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  handleSearch = (query = this.state.query) => {
-    this.setState(() => ({
-      isLoading: true,
-    }))
-    BooksAPI.search(query, MAX_SEARCH_RESULT)
+  const handleSearch = (searchTerm = query) => {
+    setIsLoading(true)
+    BooksAPI.search(searchTerm, MAX_SEARCH_RESULT)
       .then((results) => {
         if (results.length > 0) {
           //loop through the books in the shelves
@@ -34,17 +29,13 @@ export default class SearchScreen extends Component {
               }
             }
           }
-          this.setState(() => ({
-            searchResult: results,
-            isLoading: false,
-          }))
+          setIsLoading(false)
+          setSearchResult(results)
         }
       })
       .catch((err) => {
-        this.setState(() => ({
-          isLoading: false,
-          searchResult: [],
-        }))
+        setIsLoading(false)
+        setSearchResult([])
         console.warn('search error', err)
         toast.error(`No books found for the keywoard - ${query}`, {
           position: toast.POSITION.TOP_RIGHT,
@@ -52,67 +43,53 @@ export default class SearchScreen extends Component {
       })
   }
 
-  handleQueryChange = (query) => {
-    this.setState(() => ({
-      query,
-    }))
-  }
-
-  handleShelfChange = (book, newShelf, newShelfTitle) => {
-    this.setState(() => ({
-      isLoading: true,
-    }))
+  const handleShelfChange = (book, newShelf, newShelfTitle) => {
+    setIsLoading(true)
     BooksAPI.updateAsync(book, newShelf)
       .then(() => {
         toast.success(`${book.title} successfully moved to ${newShelfTitle}`, {
           position: toast.POSITION.TOP_RIGHT,
         })
-        this.setState(() => ({
-          isLoading: false,
-        }))
+        setIsLoading(false)
       })
       .catch((error) => {
-        this.setState(() => ({
-          isLoading: false,
-        }))
+        setIsLoading(false)
         toast.error(`An Error occured. Please try again later.`, {
           position: toast.POSITION.TOP_RIGHT,
         })
       })
   }
-
-  render() {
-    const { isLoading } = this.state
-    return (
-      <div className="search-books">
-        {isLoading === true && <Loading />}
-        <div className="search-books-bar">
-          <Link className="close-search" to="/">
-            Close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <form
-              onSubmit={(event) => {
-                event.preventDefault()
-                this.handleSearch()
+  return (
+    <div className="search-books">
+      {isLoading === true && <Loading />}
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault()
+              handleSearch()
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => {
+                setQuery(event.target.value)
+                console.log('query', query)
               }}
-            >
-              <input
-                type="text"
-                placeholder="Search by title or author"
-                value={this.state.query}
-                onChange={(event) => this.handleQueryChange(event.target.value)}
-              />
-            </form>
-          </div>
-        </div>
-        <div className="search-books-results">
-          <BookList
-            books={this.state.searchResult}
-            onShelfChange={this.handleShelfChange}
-          />
+            />
+          </form>
         </div>
       </div>
-    )
-  }
+      <div className="search-books-results">
+        <BookList books={searchResult} onShelfChange={handleShelfChange} />
+      </div>
+    </div>
+  )
 }
+
+export default SearchScreen
